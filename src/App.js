65b057a5f1f6453a6bef9c8203219e40e8b58a5b
Blaire <ogoh_blessing@yahoo.com>
2022-02-10 05:11:56 +0100

    Styled header component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
+
 import './App.css';
+import logo from './header-logo.png';
+
+const useStyles = makeStyles({
+  containerBox : {
+    display:"flex",
+    justifyContent: "space-between"
+  },
+  childBox : {
+    paddingRight: "4em",
+    alignSelf: "center"
+  }
+
+}
+)
 
 function App() {
+  const classes = useStyles ();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Box className={classes.containerBox}>
+          <Box>
+            <img src={logo} className="App-logo" alt="logo"></img>
+          </Box>
+          <Box className={classes.containerBox}>
+            <Box className={classes.childBox}>
+              Home
+            </Box>
+            <Box className={classes.childBox}>
+              About Us
+            </Box>
+            <Box className={classes.childBox}>
+              Our Services
+            </Box>
+            <Box className={classes.childBox}>
+              What to expect
+            </Box>
+            <Box className={classes.childBox}>
+              Feedback
+            </Box>
+            <Box className={classes.childBox}>
+              Contact
+            </Box>
+          </Box>
+        </Box>
       </header>
     </div>
   );
